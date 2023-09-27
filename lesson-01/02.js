3f66d5ca55a7c4617a2e5c0f399ffe5e84dc8a2c
Soprano7 <144703389+Soprano7@users.noreply.github.com>
2023-09-27 15:58:18 +0300

    fix again

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,11 @@
-//code
+const number1 = Math.floor(Math.random(10000) * 100);
+const evenMsg = 'четное число';
+const oddMsg = 'нечетное число';
+if (number1 % 2 === 0) 
+{
+    console.log(`${number1} :Это четное число`); 
+}
+else
+{ 
+    console.log(`${number1} :Это нечетное число`); 
+}
--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,11 +1,1 @@
-const number1 = Math.floor(Math.random(10000) * 100);
-const evenMsg = 'четное число';
-const oddMsg = 'нечетное число';
-if (number1 % 2 === 0) 
-{
-    console.log(`${number1} :Это четное число`); 
-}
-else
-{ 
-    console.log(`${number1} :Это нечетное число`); 
-}
+//code 
