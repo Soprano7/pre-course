a4541e43a0649ff7e5cd42e4f011ec56d19cff5e
Soprano7 <144703389+Soprano7@users.noreply.github.com>
2023-09-28 15:39:50 +0300

    commit

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,11 +1,6 @@
 let number = 1;
-    number+=0
-console.log(number);
-    number+=2
-console.log(number);
-    number+=0
-console.log(number);
-    number-=2
-console.log(number);
-    
+console.log(number++);
+console.log (++number)
+console.log (number--)
+console.log (--number)
     
